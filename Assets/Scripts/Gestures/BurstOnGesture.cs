ebe7b5fc6e8483074fd355582c6e942fa8ca5d6e
GSOsborne <gs.osborne.pro@gmail.com>
2020-09-19 18:33:23 -0400

    Made the controllers give much clearer visual feedback by having them reflect both the current tempo and the current layer visually. Also made the burst particles specific to the wall color. Fixed one of the green corner particles. Controller particles are now a mesh of the box and rainbowy.

--- Assets/Scripts/Gestures/BurstOnGesture.cs
+++ Assets/Scripts/Gestures/BurstOnGesture.cs
@@ -7,6 +7,7 @@ public class BurstOnGesture : MonoBehaviour
 {
     public bool checkIfLH;
     ParticleSystem pStm;
+    public Gradient redGradient, orangeGradient, yellowGradient, greenGradient, blueGradient, purpleGradient;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +23,33 @@ public class BurstOnGesture : MonoBehaviour
 
     void Burst(MusicLayer mLayer)
     {
+        var pStmMain = pStm.main;
         if (OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LTouch).magnitude > OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch).magnitude)
         {
             if (checkIfLH)
             {
+                switch (mLayer)
+                {
+                    case MusicLayer.Dholak:
+                        pStmMain.startColor = redGradient;
+                        break;
+                    case MusicLayer.Squeaker:
+                        pStmMain.startColor = orangeGradient;
+                        break;
+                    case MusicLayer.Charleston:
+                        pStmMain.startColor = yellowGradient;
+                        break;
+                    case MusicLayer.Sniper:
+                        pStmMain.startColor = greenGradient;
+                        break;
+                    case MusicLayer.OGProd:
+                        pStmMain.startColor = blueGradient;
+                        break;
+                    case MusicLayer.Funk:
+                        pStmMain.startColor = purpleGradient;
+                        break;
+                }
+
                 pStm.Play();
             }
         }
@@ -33,6 +57,27 @@ public class BurstOnGesture : MonoBehaviour
         {
             if (!checkIfLH)
             {
+                switch (mLayer)
+                {
+                    case MusicLayer.Dholak:
+                        pStmMain.startColor = redGradient;
+                        break;
+                    case MusicLayer.Squeaker:
+                        pStmMain.startColor = orangeGradient;
+                        break;
+                    case MusicLayer.Charleston:
+                        pStmMain.startColor = yellowGradient;
+                        break;
+                    case MusicLayer.Sniper:
+                        pStmMain.startColor = greenGradient;
+                        break;
+                    case MusicLayer.OGProd:
+                        pStmMain.startColor = blueGradient;
+                        break;
+                    case MusicLayer.Funk:
+                        pStmMain.startColor = purpleGradient;
+                        break;
+                }
                 pStm.Play();
             }
         }
